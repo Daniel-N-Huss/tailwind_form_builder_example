24d6a2998e238bc7724154eb0e17bdf801de2734
Daniel Huss <63384617+Daniel-N-Huss@users.noreply.github.com>
2022-11-04 16:16:41 -0600

    migrate to adventurers

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_04_220739) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "adventurers", force: :cascade do |t|
+    t.string "name"
+    t.string "race"
+    t.string "alignment"
+    t.string "city"
+    t.string "primary_weapon"
+    t.string "secondary_weapon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
