9c566b93fac87558ee44e8482188da8fc3fcae42
Daniel Huss <63384617+Daniel-N-Huss@users.noreply.github.com>
2022-11-07 14:01:57 -0700

    add formbuilder styles to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
+    './app/lib/form_builders/**/*.rb',
     './app/javascript/**/*.js'
   ]
 }
