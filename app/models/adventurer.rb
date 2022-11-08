6743458329d1f7d415bb1182f40b55b89e75bda2
Daniel Huss <63384617+Daniel-N-Huss@users.noreply.github.com>
2022-11-08 16:02:33 -0700

    move alignments to const, with validation

--- app/models/adventurer.rb
+++ app/models/adventurer.rb
@@ -1,3 +1,13 @@
 class Adventurer < ApplicationRecord
   validates_presence_of :name, :city
+  validate :good_alignment
+
+  EVIL_ALIGNMENTS = ["Lawful Evil", "Neutral Evil", "Chaotic Evil"].freeze
+  ALIGNMENTS = (["Lawful Good", "Neutral Good", "Chaotic Good", "Lawful Neutral", "Neutral", "Chaotic Neutral"] + EVIL_ALIGNMENTS).freeze
+
+  def good_alignment
+    if EVIL_ALIGNMENTS.include?(alignment)
+      errors.add(:alignment, "No Evil Characters")
+    end
+  end
 end
