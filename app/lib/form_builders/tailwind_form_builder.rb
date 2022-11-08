0dd5e57f6f5b1155dd2650543f57a65f2c5571c5
Daniel Huss <63384617+Daniel-N-Huss@users.noreply.github.com>
2022-11-08 16:31:10 -0700

    code golf, lift structure styles to view

--- app/lib/form_builders/tailwind_form_builder.rb
+++ app/lib/form_builders/tailwind_form_builder.rb
@@ -4,6 +4,10 @@ module FormBuilders
     #  leans on the FormBuilder class_attribute `field_helpers`
     #  you'll want to add a method for each of the specific helpers listed here if you want to style them
 
+    TEXT_FIELD_STYLE = "bg-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white".freeze
+    SELECT_FIELD_STYLE = "block bg-gray-200 text-gray-700 py-2 px-4 rounded leading-tight focus:outline-none focus:bg-white".freeze
+    SUBMIT_BUTTON_STYLE = "shadow bg-yellow-800 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded hover:bg-yellow-700".freeze
+
     text_field_helpers.each do |field_method|
       class_eval <<-RUBY_EVAL, __FILE__, __LINE__ + 1
           def #{field_method}(method, options = {})
@@ -18,28 +22,34 @@ module FormBuilders
 
     def submit(value = nil, options = {})
       custom_opts, opts = partition_custom_opts(options)
+      classes = apply_style_classes(SUBMIT_BUTTON_STYLE, custom_opts)
 
-      classes = <<~CLASSES.strip
-        shadow bg-yellow-800 focus:shadow-outline focus:outline-none text-white font-bold 
-        py-2 px-4 rounded hover:bg-yellow-700
-      CLASSES
-
-      classes += " #{custom_opts[:class]}"
       super(value, {class: classes}.merge(opts))
     end
 
     def select(method, choices = nil, options = {}, html_options = {}, &block)
       custom_opts, opts = partition_custom_opts(options)
+      classes = apply_style_classes(SELECT_FIELD_STYLE, custom_opts, method)
 
-      classes = <<~CLASSES.strip
-        block bg-gray-200 text-gray-700 py-2 px-4 rounded leading-tight focus:outline-none focus:bg-white
-      CLASSES
-
-      classes += border_color_classes(method)
-      classes += " #{custom_opts[:class]}"
-
-      field = super(method, choices, opts, html_options.merge({class: classes}), &block)
       labels = labels(method, custom_opts[:label], options)
+      field = super(method, choices, opts, html_options.merge({class: classes}), &block)
+
+      labels + field
+    end
+
+    private
+
+    def text_like_field(field_method, object_method, options = {})
+      custom_opts, opts = partition_custom_opts(options)
+
+      classes = apply_style_classes(TEXT_FIELD_STYLE, custom_opts, object_method)
+
+      field = send(field_method, object_method, {
+        class: classes,
+        title: errors_for(object_method)&.join(" ")
+      }.compact.merge(opts).merge({tailwindified: true}))
+
+      labels = labels(object_method, custom_opts[:label], options)
 
       labels + field
     end
@@ -72,28 +82,6 @@ module FormBuilders
       end
     end
 
-    private
-
-    def text_like_field(field_method, object_method, options = {})
-      custom_opts, opts = partition_custom_opts(options)
-
-      classes = <<~CLASSES.strip
-        min-w-1/2 bg-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white
-      CLASSES
-
-      classes += border_color_classes(object_method)
-      classes += " #{custom_opts[:class]}"
-
-      field = send(field_method, object_method, {
-        class: classes,
-        title: errors_for(object_method)&.join(" ")
-      }.compact.merge(opts).merge({tailwindified: true}))
-
-      labels = labels(object_method, custom_opts[:label], options)
-
-      labels + field
-    end
-
     def border_color_classes(object_method)
       if errors_for(object_method).present?
         " border-2 border-red-400 focus:border-rose-200"
@@ -102,13 +90,17 @@ module FormBuilders
       end
     end
 
+    def apply_style_classes(classes, custom_opts, object_method = nil)
+      classes + border_color_classes(object_method) + " #{custom_opts[:class]}"
+    end
+
     CUSTOM_OPTS = [:label, :class].freeze
     def partition_custom_opts(opts)
       opts.partition { |k, v| CUSTOM_OPTS.include?(k) }.map(&:to_h)
     end
 
     def errors_for(object_method)
-      return unless @object.present?
+      return unless @object.present? && object_method.present?
 
       @object.errors[object_method]
     end
